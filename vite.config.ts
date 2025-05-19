ab6d985d0380c3706ddefaf1106ef0210bcafb26
jozsef-sogor <sogor.jozsef98@gmail.com>
2025-05-19 14:08:31 +0200

    Add tailwind and shadcn vue

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,26 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
       '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
-      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
       '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@types': fileURLToPath(new URL('./src/types', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
     },
   },
 })
