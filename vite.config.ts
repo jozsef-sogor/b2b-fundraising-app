c5f959207c64f295b1e35224fca3478dc4d45b28
jozsef-sogor <sogor.jozsef98@gmail.com>
2025-05-19 10:42:37 +0200

    Base scaffolding

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@composables': fileURLToPath(new URL('./src/composables', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@services': fileURLToPath(new URL('./src/services', import.meta.url)),
     },
   },
 })
